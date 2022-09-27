2928a613f8d608821b10dcde4b68e45a5ce03d3a
Javier <jmartin@grupoloyal.com>
2022-09-27 10:55:21 +0200

    Repositorio

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +15,33 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
+//		System.out.println("Hola mundo");
+//		dao.findAll().forEach(System.out::println);
+//		var item = dao.findById(1111);
+//		if(item.isPresent()) {
+//			System.out.println(item.get());
+//		} else {
+//			System.out.println("No encontrado");
+//		}
+//		var item = new Actor(0, "Pepito", "Grillo");
+//		dao.save(item);
+//		dao.findAll().forEach(System.out::println);
+//		var item = dao.findById(204);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//			dao.findAll().forEach(System.out::println);
+//		} else {
+//			System.out.println("No encontrado");
+//		}
+		dao.deleteById(204);
+		dao.findAll().forEach(System.out::println);
 	}
 
 }
